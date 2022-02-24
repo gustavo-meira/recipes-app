4cd78a324f92973cd002b1cfe59fdecd99daf15d
Gustavo Meira <gmeira.antonio@gmail.com>
2022-02-24 16:32:08 -0300

    feat(loginPage): desabilita botao para logar

--- src/components/SigIn.jsx
+++ src/components/SigIn.jsx
@@ -4,7 +4,13 @@ const SignIn = () => (
   <div>
     <input data-cy="email-input" placeholder="Email" />
     <input data-cy="password-input" placeholder="Senha" />
-    <button data-cy="login-submit-button" type="button">Logar</button>
+    <button
+      data-cy="login-submit-button"
+      type="button"
+      disabled
+    >
+      Logar
+    </button>
   </div>
 );
 
