e4ed77568e63df5e279fe5a83c83e36383200b28
Gustavo Meira <gmeira.antonio@gmail.com>
2022-02-24 15:31:22 -0300

    tests: testes da pagina de login

--- cypress/integration/login_page.spec.js
+++ cypress/integration/login_page.spec.js
@@ -1,6 +1,12 @@
 const EMAIL_INPUT_TEST_ID = '[data-cy="email-input"]';
 const PASSWORD_INPUT_TEST_ID = '[data-cy="password-input"]';
 const LOGIN_BUTTON_TEST_ID = '[data-cy="login-submit-button"]';
+const VALID_EMAIL = 'email@email.com';
+const INVALID_EMAIL_1 = 'email@email';
+const INVALID_EMAIL_2 = 'email.com';
+const VALID_PASSWORD = '123456';
+const INVALID_PASSWORD = '123';
+const EMAIL_LOCAL_STORAGE = JSON.stringify({ email: VALID_EMAIL });
 
 describe('Testa os componentes da pagina de login', () => {
   beforeEach(() => {
@@ -11,5 +17,48 @@ describe('Testa os componentes da pagina de login', () => {
     cy.get(EMAIL_INPUT_TEST_ID);
     cy.get(PASSWORD_INPUT_TEST_ID);
     cy.get(LOGIN_BUTTON_TEST_ID);
+  });
+
+  it('É possivel escrever nos campos de input', () => {
+    cy.get(EMAIL_INPUT_TEST_ID).type(VALID_EMAIL);
+    cy.get(PASSWORD_INPUT_TEST_ID).type(VALID_PASSWORD);
+  });
+
+  it('O botão inicia como desabilitado', () => {
+    cy.get(LOGIN_BUTTON_TEST_ID).should('be.disabled');
+  });
+
+  it('Se for escrito um email e senha invalidos, o botão devera continuar desabilitado', () => {
+    cy.get(EMAIL_INPUT_TEST_ID).type(INVALID_EMAIL_1);
+    cy.get(PASSWORD_INPUT_TEST_ID).type(INVALID_PASSWORD);
+    cy.get(LOGIN_BUTTON_TEST_ID).should('be.disabled');
+
+    cy.get(EMAIL_INPUT_TEST_ID).clear();
+    cy.get(EMAIL_INPUT_TEST_ID).type(INVALID_EMAIL_2);
+    cy.get(LOGIN_BUTTON_TEST_ID).should('be.disabled');
+  });
+
+  it('Se os dados forem inseridos corretamente, o botão devera ficar habilitado', () => {
+    cy.get(EMAIL_INPUT_TEST_ID).type(VALID_EMAIL);
+    cy.get(PASSWORD_INPUT_TEST_ID).type(VALID_PASSWORD);
+    cy.get(LOGIN_BUTTON_TEST_ID).should('be.enabled');
+  });
+
+  it('O botão de logar deve salvar o email do usuario no localStorage na chave user', () => {
+    cy.clearLocalStorage();
+
+    cy.get(EMAIL_INPUT_TEST_ID).type(VALID_EMAIL);
+    cy.get(PASSWORD_INPUT_TEST_ID).type(VALID_PASSWORD);
+    cy.get(LOGIN_BUTTON_TEST_ID).click();
+
+    cy.getLocalStorage('user').should('equal', EMAIL_LOCAL_STORAGE);
+  });
+
+  it.only('O botão de logar deve levar o usuario para a pagina "/foods"', () => {
+    cy.get(EMAIL_INPUT_TEST_ID).type(VALID_EMAIL);
+    cy.get(PASSWORD_INPUT_TEST_ID).type(VALID_PASSWORD);
+    cy.get(LOGIN_BUTTON_TEST_ID).click();
+
+    cy.url().should('equal', 'http://localhost:3000/foods');
   });
 });
