958163bb472febac96b85ad86194c3000b97af43
Gustavo Meira <gmeira.antonio@gmail.com>
2022-02-24 18:30:35 -0300

    tests: criando utils para controlar as rotas

---
+++ cypress/utils/pages.js
@@ -0,0 +1,17 @@
+
+export const LOGIN_PAGE = 'http://localhost:3000/';
+export const FOODS_PAGE = 'http://localhost:3000/foods';
+export const DRINKS_PAGE = 'http://localhost:3000/drinks';
+export const FOODS_DETAILS_PAGE = (id) => `http://localhost:3000/foods/${id}`;
+export const DRINKS_DETAILS_PAGE = (id) => `http://localhost:3000/drinks/${id}`;
+export const FOODS_IN_PROGRESS_PAGE = (id) => `http://localhost:3000/foods/${id}/in-progress`;
+export const DRINKS_IN_PROGRESS_PAGE = (id) => `http://localhost:3000/drinks/${id}/in-progress`;
+export const DONE_RECIPES_PAGE = 'http://localhost:3000/done-recipes';
+export const FAVORITE_RECIPES_PAGE = 'http://localhost:3000/favorite-recipes';
+export const EXPLORE_RECIPES_PAGE = 'http://localhost:3000/explore';
+export const EXPLORE_FOODS_PAGE = 'http://localhost:3000/explore/foods';
+export const EXPLORE_DRINKS_PAGE = 'http://localhost:3000/explore/drinks';
+export const EXPLORE_FOODS_BY_INGREDIENT_PAGE = 'http://localhost:3000/explore/foods/ingredients';
+export const EXPLORE_DRINKS_BY_INGREDIENT_PAGE = 'http://localhost:3000/explore/drinks/ingredients';
+export const EXPLORE_FOODS_BY_NATIONALITY_PAGE = 'http://localhost:3000/explore/foods/nationalities';
+export const PROFILE_PAGE = 'http://localhost:3000/profile';
