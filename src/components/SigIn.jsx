e472ed6b434c6899757527b1b4ee5ea60da96d0c
Gustavo Meira <gmeira.antonio@gmail.com>
2022-02-24 16:43:52 -0300

    feat(loginPage): habilita botao de login caso a pessoa usuaria digite um email e senha validos

--- src/components/SigIn.jsx
+++ src/components/SigIn.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SignIn = () => (
-  <div>
-    <input data-cy="email-input" placeholder="Email" />
-    <input data-cy="password-input" placeholder="Senha" />
-    <button
-      data-cy="login-submit-button"
-      type="button"
-      disabled
-    >
-      Logar
-    </button>
-  </div>
-);
+const SignIn = () => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const testInputs = () => {
+    const MIN_LENGTH_PASSWORD = 6;
+
+    return !(
+      userEmail.includes('@') && userEmail.includes('.com')
+      && userPassword.length >= MIN_LENGTH_PASSWORD
+    );
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        data-cy="email-input"
+        placeholder="Email"
+        value={ userEmail }
+        onChange={ ({ target: { value } }) => setUserEmail(value) }
+      />
+      <input
+        type="password"
+        data-cy="password-input"
+        placeholder="Senha"
+        value={ userPassword }
+        onChange={ ({ target: { value } }) => setUserPassword(value) }
+      />
+      <button
+        data-cy="login-submit-button"
+        type="button"
+        disabled={ testInputs() }
+      >
+        Logar
+      </button>
+    </div>
+  );
+};
 
 export default SignIn;
