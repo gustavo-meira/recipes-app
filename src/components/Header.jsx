7b006e325a0464d95cb3601fd1e9e435484e7335
Gustavo Meira <gmeira.antonio@gmail.com>
2022-02-28 11:38:48 -0300

    feat(header): botao de busca agora e opcional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import userIcon from '../assets/images/user.svg';
 import searchIcon from '../assets/images/search-icon.svg';
 
-const Header = () => (
-  <header>
-    <img src={ userIcon } alt="Icone do usuario" data-cy="profile-button" />
-    <h2 data-cy="app-title">Recipes App</h2>
-    <img src={ searchIcon } alt="Icone de busca" data-cy="search-button" />
-  </header>
-);
+const Header = ({ searchButton }) => {
+  
+  
+  return (
+    <header>
+      <img src={ userIcon } alt="Icone do usuario" data-cy="profile-button" />
+      <h2 data-cy="app-title">Recipes App</h2>
+      {
+        searchButton && (
+          <img src={ searchIcon } alt="Icone de busca" data-cy="search-button" />
+        )
+      }
+    </header>
+  );
+};
+
+Header.propTypes = {
+  searchButton: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  searchButton: true,
+};
 
 export default Header;
