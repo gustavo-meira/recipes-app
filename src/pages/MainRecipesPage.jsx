b50955f9847960e873cf5ab4536979b6ef214bb2
Gustavo Meira <gmeira.antonio@gmail.com>
2022-03-05 09:33:26 -0300

    feat: Adicionando footer a pagina principal

--- src/pages/MainRecipesPage.jsx
+++ src/pages/MainRecipesPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import CategoryList from '../components/CategoryList';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipeList from '../components/RecipeList';
 
 const MainRecipesPage = () => (
   <>
-    <h1>Pagina principal das receitas de comidas e bebidas</h1>
     <Header />
     <CategoryList />
     <RecipeList />
+    <Footer />
   </>
 );
 
