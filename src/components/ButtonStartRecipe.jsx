fc1c396458a604f74808205ee593ff9e5a0513ac
Gustavo Meira <gmeira.antonio@gmail.com>
2022-03-10 09:12:36 -0300

    feat(ButtonStartRecipe): O botao se atualiza conforme a pessoa ja tenha feito a receita ou ela esteja em progresso

--- src/components/ButtonStartRecipe.jsx
+++ src/components/ButtonStartRecipe.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import RecipeDetailContext from '../contexts/RecipeDetailContext';
 
-const ButtonStartRecipe = () => (
-  <button type="button">Iniciar Receita</button>
-);
+const ButtonStartRecipe = () => {
+  const location = useLocation();
+  const recipe = useContext(RecipeDetailContext);
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+
+  const recipeDone = doneRecipes.some(({ id }) => recipe.id === id);
+  if (recipeDone) return null;
+
+  let textButton = 'Iniciar Receita';
+
+  const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
+  const recipeInProgress = inProgressRecipes.some(({ id }) => recipe.id === id);
+  if (recipeInProgress) textButton = 'Continuar Receita';
+
+  return (
+    <Link to={ `${location.pathname}/in-progress` }>
+      <button type="button">{ textButton }</button>
+    </Link>
+  );
+};
 
 export default ButtonStartRecipe;
