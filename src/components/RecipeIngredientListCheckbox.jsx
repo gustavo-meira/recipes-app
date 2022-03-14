33bbf2db60f4f7c5f0dd7c7f5e74caaa57871887
Gustavo Meira <gmeira.antonio@gmail.com>
2022-03-14 10:04:15 -0300

    feat(RecipeIngredientListCheckbox): Ao clicar em uma checkbox seu progresso e salvo no localStorage

--- src/components/RecipeIngredientListCheckbox.jsx
+++ src/components/RecipeIngredientListCheckbox.jsx
@@ -4,6 +4,26 @@ import RecipeDetailContext from '../contexts/RecipeDetailContext';
 const RecipeIngredientListCheckbox = () => {
   const recipe = useContext(RecipeDetailContext);
 
+  const handleCheckInput = (event) => {
+    const { target: { id: ingredientId, checked } } = event;
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
+    const recipeInProgress = inProgressRecipes.some(
+      (inProgressRecipe) => inProgressRecipe.id === recipe.id,
+    );
+    if (recipeInProgress) {
+      const currIndex = inProgressRecipes.findIndex(({ id }) => id === recipe.id);
+      inProgressRecipes[currIndex] = {
+        ...inProgressRecipes[currIndex], [ingredientId]: checked,
+      };
+    } else {
+      inProgressRecipes.push({
+        id: recipe.id,
+        [ingredientId]: checked,
+      });
+    }
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+  };
+
   return (
     <ul>
       {
@@ -11,7 +31,7 @@ const RecipeIngredientListCheckbox = () => {
           <li key={ index }>
             <label htmlFor={ `ingredient-${index}` }>
               <input
-                onChange={ (e) => console.log(e.target.checked) }
+                onChange={ handleCheckInput }
                 id={ `ingredient-${index}` }
                 type="checkbox"
               />
