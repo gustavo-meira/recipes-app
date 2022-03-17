858f847b044e40453f517cc851598c59a30d1e7c
Gustavo Meira Antonio <gmeira.antonio@gmail.com>
2022-03-17 09:31:33 -0300

    feat(InProgressRecipePage): Adiciona instruções da receita a pagina

--- src/pages/InProgressRecipePage.jsx
+++ src/pages/InProgressRecipePage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import RecipeDetailProvider from '../contexts/RecipeDetailProvider';
 import RecipeHeader from '../components/RecipeHeader';
 import RecipeIngredientListCheckbox from '../components/RecipeIngredientListCheckbox';
+import RecipeInstrucions from '../components/RecipeInstructions';
 
 const InProgressRecipePage = () => (
   <RecipeDetailProvider>
     <RecipeHeader />
+    <RecipeInstrucions />
     <RecipeIngredientListCheckbox />
   </RecipeDetailProvider>
 );
