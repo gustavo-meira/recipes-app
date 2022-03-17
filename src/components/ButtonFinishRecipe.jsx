70d1000bd175dfd75e7bd5032ad49dee556fdd42
Gustavo Meira Antonio <gmeira.antonio@gmail.com>
2022-03-17 09:24:49 -0300

    feat(ButtonFinishRecipe): Ao clicar no botão a receita sai da parte de em progresso e passa para as receitas concluidas

--- src/components/ButtonFinishRecipe.jsx
+++ src/components/ButtonFinishRecipe.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import RecipeDetailContext from '../contexts/RecipeDetailContext';
 
-const ButtonFinishRecipe = ({ allIngredientsDones }) => (
-  <button type="button" disabled={ !allIngredientsDones }>
-    Finalizar Receita
-  </button>
-);
+const ButtonFinishRecipe = ({ allIngredientsDones }) => {
+  const recipe = useContext(RecipeDetailContext);
+
+  const deleteRecipeInProgress = () => {
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    const recipes = inProgressRecipes.filter(({ id }) => recipe.id !== id);
+    localStorage.setItem('inProgressRecipes', JSON.stringify(recipes));
+  };
+
+  const handleButtonFinishRecipe = () => {
+    const { id, thumb, name } = recipe;
+    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    doneRecipes.push({ id, thumb, name });
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+    deleteRecipeInProgress();
+  };
+
+  return (
+    <button
+      onClick={ handleButtonFinishRecipe }
+      type="button"
+      disabled={ !allIngredientsDones }
+    >
+      Finalizar Receita
+    </button>
+  );
+};
 
 ButtonFinishRecipe.propTypes = {
   allIngredientsDones: PropTypes.bool.isRequired,
