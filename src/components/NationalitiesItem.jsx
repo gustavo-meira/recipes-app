ca66355535b1aa13171c263ddaad745e1cb99911
Gustavo Meira Antonio <gmeira.antonio@gmail.com>
2022-03-25 12:38:33 -0300

    feat(NationalitiesItem): Ao clicar em uma nacionalidade é levado para a pagina de comidas com a nacionalidade escolhida

--- src/components/NationalitiesItem.jsx
+++ src/components/NationalitiesItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const NationalitiesItem = ({ nationality }) => (
   <li>
-    { nationality }
+    <Link to={ `/foods?nationality=${nationality}` }>
+      { nationality }
+    </Link>
   </li>
 );
 
