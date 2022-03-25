e10766e04f51c732f571179ff909cc89c4aee113
Gustavo Meira Antonio <gmeira.antonio@gmail.com>
2022-03-25 10:23:38 -0300

    feat(IngredientItem): Ao clicar em um ingrediente é levado para a pagina de receitas com aquele ingrediente

--- src/components/IngredientItem.jsx
+++ src/components/IngredientItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 
-const IngredientItem = ({ ingredient }) => (
-  <li>
-    { ingredient }
-  </li>
-);
+const IngredientItem = ({ ingredient }) => {
+  const { pathname } = useLocation();
+  const recipeType = pathname.includes('foods') ? 'foods' : 'drinks';
+
+  return (
+    <li>
+      <Link to={ `/${recipeType}?ingredient=${ingredient}` }>
+        { ingredient }
+      </Link>
+    </li>
+  );
+};
 
 IngredientItem.propTypes = {
   ingredient: PropTypes.string.isRequired,
