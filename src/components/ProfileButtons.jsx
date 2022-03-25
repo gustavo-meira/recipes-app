1f52fcf9aa092c737a6304107852205e084aa269
Gustavo Meira Antonio <gmeira.antonio@gmail.com>
2022-03-25 13:36:54 -0300

    fix(ProfileButtons): Se o email não for encontrado aparecerá um erro avisando

--- src/components/ProfileButtons.jsx
+++ src/components/ProfileButtons.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ProfileButtons = () => {
-  const { email } = JSON.parse(localStorage.getItem('user'));
+  const { email } = JSON.parse(localStorage.getItem('user')) || { email: '' };
 
   return (
     <div>
-      <h2>{ email }</h2>
+      <h2>{ email || 'Email não encontrado' }</h2>
       <button type="button">Done Recipes</button>
       <button type="button">Favorite Recipes</button>
       <button type="button">Logout</button>
